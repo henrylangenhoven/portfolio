0bd27b2881b74ca44a82d02500dd15558832a074
henrylangman <henry.l.pta@gmail.com>
2020-05-16 11:05:21 +0200

    adding ng-mocks and adding MockComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,27 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MockComponent } from 'ng-mocks';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MastheadComponent } from './masthead/masthead.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactRequestComponent } from './contact/contact-request/contact-request.component';
+import { ContactComponent } from './contact/contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [
+        AppComponent,
+        MockComponent(NavigationComponent),
+        MockComponent(MastheadComponent),
+        MockComponent(AboutComponent),
+        MockComponent(ProjectsComponent),
+        MockComponent(ContactRequestComponent),
+        MockComponent(ContactComponent),
+        MockComponent(FooterComponent),
+      ],
     }).compileComponents();
   }));
 
