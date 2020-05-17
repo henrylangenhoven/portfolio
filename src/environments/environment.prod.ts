0f3bdc81cab94e9f7022f6fc22cdb8f405024bf5
henrylangman <henry.l.pta@gmail.com>
2020-05-17 23:39:16 +0200

    adding firebase config placeholders

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: '<<private-information>>',
+    authDomain: '<<private-information>>',
+    databaseURL: '<<private-information>>',
+    projectId: '<<private-information>>',
+    storageBucket: '<<private-information>>',
+    messagingSenderId: '<<private-information>>',
+    appId: '<<private-information>>',
+    measurementId: '<<private-information>>',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: '<<private-information>>',
+    authDomain: '<<private-information>>',
+    databaseURL: '<<private-information>>',
+    projectId: '<<private-information>>',
+    storageBucket: '<<private-information>>',
+    messagingSenderId: '<<private-information>>',
+    appId: '<<private-information>>',
+    measurementId: '<<private-information>>',
+  },
 };
 
 /*
